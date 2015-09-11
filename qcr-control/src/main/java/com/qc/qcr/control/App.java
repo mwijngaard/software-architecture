c605a7a4d8383bb3a96af955a9a5e5879cc467c6
NickLodewijks <NickLodewijks@gmail.com>
2015-09-12 01:10:57 +0200

    Initial commit of directory structure

---
+++ qcr-control/src/main/java/com/qc/qcr/control/App.java
@@ -0,0 +1,13 @@
+package com.qc.qcr.control;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ qcr-util/src/main/java/com/qc/qcr/util/App.java
@@ -0,0 +1,13 @@
+package com.qc.qcr.util;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
