4b61b638dd77b330bd8801b46fe9f43ec929f7bd
NickLodewijks <NickLodewijks@gmail.com>
2015-09-17 15:53:14 +0200

    Added implementation of a blocking DataPipe

---
+++ qcr-common/src/main/java/com/qc/qcr/common/pf/BlockingQueueDataPipe.java
@@ -0,0 +1,52 @@
+package com.qc.qcr.common.pf;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+public class BlockingQueueDataPipe<T> implements DataPipe<T> {
+
+	private final BlockingQueue<T> dataQueue;
+	private boolean isClosed;
+
+	public BlockingQueueDataPipe() {
+		dataQueue = new LinkedBlockingQueue<T>();
+	}
+
+	@Override
+	public T read() {
+		T data;
+
+		data = null;
+
+		while (!dataQueue.isEmpty() || !isClosedForWriting()) {
+			try {
+				data = dataQueue.poll(1000, TimeUnit.MILLISECONDS);
+
+				if (data != null) {
+					return data;
+				}
+			} catch (InterruptedException ex) {
+				// TODO: Do something with this later
+				return null;
+			}
+		}
+
+		return data;
+	}
+
+	@Override
+	public void write(T data) {
+		dataQueue.add(data);
+	}
+
+	public synchronized boolean isClosedForWriting() {
+		return isClosed;
+	}
+
+	@Override
+	public synchronized void closeForWriting() {
+		isClosed = true;
+	}
+
+}
