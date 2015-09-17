2b23a8ea07b0ef1bf0618524434e7d5dd632953c
Julian Jansen <julian.jansen@gmail.com>
2015-09-17 13:52:43 +0200

    Basic number generator

---
+++ qcr-common/src/test/java/com/qc/qcr/common/pf/NumberGenerator.java
@@ -0,0 +1,45 @@
+package com.qc.qcr.common.pf;
+
+import java.util.Random;
+
+public class NumberGenerator extends AbstractActiveDataSource<Integer> {
+
+	public NumberGenerator(DataPipe<Integer> outputPipe) {
+		super(outputPipe);
+	}
+
+	public void generateInto(DataPipe<Integer> outputPipe) {
+		
+		Integer number = 80;
+		Integer seed = 5;
+		Integer delta;
+		
+		Random randomGenerator = new Random();
+		
+		for (int i = 0; i < 1000; i++) {
+			
+			delta = randomGenerator.nextInt(seed) % 10;
+
+			if (i % 2 == 0) {
+				number -= delta;
+			} else {
+				number += delta;
+			}
+			
+			System.out.println(number);
+			
+			outputPipe.write(number);
+		}
+		
+		
+		
+	//	outputPipe.write(1);
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		NumberGenerator ng = new NumberGenerator(null);
+		ng.generateInto(null);
+	}
+}
